d8a59ed3e09d67e175ecf508092079ebececc47d
Kyle Carlson <thekylecarlson@gmail.com>
2022-03-31 20:16:54 -0500

    model validations (untested)

--- app/models/short_link.rb
+++ app/models/short_link.rb
@@ -1,2 +1,15 @@
 class ShortLink < ApplicationRecord
+  ## Callbacks
+  before_validation { full_url.downcase! }
+
+  ## Validations
+  validates :full_url,  presence: true, 
+                        format: URI::regexp(%w[http https])
+                        uniqueness: true  # since we downcase it before validations are run. 
+
+  # case doesn't matter because uppercase is totally different than lowercase
+  # i.e. ci.com/aBc != ci.com/abc
+  validates :short_link, presence: true, 
+                         uniqueness: true,
+                         format: URI::regexp(%w[http https])
 end
