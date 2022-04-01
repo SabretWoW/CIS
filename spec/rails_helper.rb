47cb8fe91f8b4f9c6947ae298f779f95a71ba6aa
Kyle Carlson <thekylecarlson@gmail.com>
2022-04-01 09:07:48 -0500

    rails_helper and spec_helper setup

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,15 @@ require_relative '../config/environment'
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'database_cleaner-active_record'
+# require 'capybara/rails'
+require 'capybara/rspec'
+require 'capybara-screenshot/rspec'
+# require 'devise'
+
+Dir[Rails.root.join("spec", "support", "**", "*.rb")].sort.each { |f| require f }
+
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -22,22 +31,14 @@ require 'rspec/rails'
 #
 # Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
 
-# Checks for pending migrations and applies them before tests are run.
-# If you are not using ActiveRecord, you can remove these lines.
-begin
-  ActiveRecord::Migration.maintain_test_schema!
-rescue ActiveRecord::PendingMigrationError => e
-  puts e.to_s.strip
-  exit 1
-end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  # config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  # config.use_transactional_fixtures = false
 
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
@@ -61,4 +62,44 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  
+  # https://github.com/DatabaseCleaner/database_cleaner#test-framework-examples
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+  
+  config.after :each, type: :feature do |example|
+    Capybara::Screenshot::RSpec.after_failed_example(example)
+  end
+  
+  config.include Capybara::DSL
+
+  config.include FactoryBot::Syntax::Methods
+
+  # config.include Devise::Test::IntegrationHelpers, type: :request
+  # config.include Devise::Test::ControllerHelpers, type: :controller
+  # config.include Warden::Test::Helpers
+  config.use_transactional_fixtures = true
+
+  # config.after(:each) do |example|
+  #   if example.exception
+  #     file_name = 'failed_%s.png' % rand(1000).to_s # name your screenshot whatever you'd like
+  #     page.save_screenshot(file_name)
+  #   end
+  # end
+end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
 end
