91c2fcaa4d6cea3a2ffa82e96955c905fe3fba78
Kyle Carlson <thekylecarlson@gmail.com>
2022-03-31 22:45:49 -0500

    generates shortlink!!!

--- app/models/short_link.rb
+++ app/models/short_link.rb
@@ -2,11 +2,12 @@ class ShortLink < ApplicationRecord
   ## Callbacks
   before_validation do
     self.full_url.downcase!
+    generate_short_suffix
   end
 
   ## Validations
   validates :full_url,  presence: true, 
-                        format: URI::regexp(%w[http https])
+                        format: URI::regexp(%w[http https]),
                         uniqueness: true
 
   # case doesn't matter because uppercase is totally different than lowercase
@@ -14,4 +15,21 @@ class ShortLink < ApplicationRecord
   validates :short_link, presence: true, 
                          uniqueness: true,
                          format: URI::regexp(%w[http https])
+
+
+  ## Methods
+  def generate_short_suffix
+    lowercase_letters = "a".."z"
+    uppercase_letters = "A".."Z"
+    alphabet = lowercase_letters.zip(uppercase_letters).flatten # "a", "A", "b", "B", "c", "C", ..
+    link_suffix = ""
+
+    10.times do
+      character_index = rand(0..51)
+      character_to_add = alphabet[character_index]
+      link_suffix = link_suffix + character_to_add
+    end
+
+    self.short_link = "https://ci.com/" + link_suffix
+  end
 end
